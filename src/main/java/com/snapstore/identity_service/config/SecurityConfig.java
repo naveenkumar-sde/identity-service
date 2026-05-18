a06dd01dca7746d318bd0bf519d5b7b14e667f49
Naveen <naveenkumar.sde@gmail.com>
2026-05-18 11:13:40 +0530

    feat: adding global exceptions

--- src/main/java/com/snapstore/identity_service/config/SecurityConfig.java
+++ src/main/java/com/snapstore/identity_service/config/SecurityConfig.java
@@ -17,8 +17,8 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest()
-                        .authenticated()
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated()
                 );
 
         return http.build();
